0f69a91baeadca9736d21c9b5b6eb8b2ae9e4bae
ChrisMent <120587060+ChrisMent@users.noreply.github.com>
2022-12-14 18:15:22 +0100

    text adjustment

--- main.js
+++ main.js
@@ -3,15 +3,20 @@ function check(){
 
     let input = document.getElementById("age")
     let age = input.value
+
   
     // < 6 Jahre oder > 60 --> Sonderpreis 4€
 
-    if(age <= 6 || age >= 60){
+    if(age < 0 || age > 110){
+        alert("Kein gültiges Alter!")
+    }
+    
+    else if(age <= 6 || age >= 60){
 
         alert("Der Ticketpreis beträgt 4€")
 
     // Für alle anderen --> 6.50€
-    
+
     } else {
         alert("Der Ticketpreis beträgt 6.50€")
 
